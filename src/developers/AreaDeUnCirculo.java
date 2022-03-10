8a1a3a857f3ef6b0d553bfe852a8a764ff7713e4
devgamez2022 <winmanuel09@gmail.com>
2022-03-10 10:39:51 -0600

    Clase java Área de un circulo

---
+++ src/developers/AreaDeUnCirculo.java
@@ -0,0 +1,17 @@
+package developers;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class AreaDeUnCirculo {
+    public static void main(String[] args) {
+        double area, radio;
+        final double pi = 3.1416;
+        
+        NumberFormat formato = new DecimalFormat("#0.00");
+        
+        area = 145;
+        radio = Math.sqrt(area/pi);
+        System.out.println("Radio: " + formato.format(radio) + " cm");
+    }
+}
